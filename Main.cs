cbd233cf764a7636705add26b5efc7d2a05a4e1a
Missury15 <tadeas.hajek.cz@gmail.com>
2025-09-24 19:29:31 +0200

    Change namespace from HelloWorld to SquareCalculator

--- Main.cs
+++ Main.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace SquareCalculator
 {
     class Program
     {
